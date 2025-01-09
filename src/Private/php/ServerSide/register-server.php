1fad351ee082dccecc4ba1a20e14524be9b2a1cb
Dante <truedevilhunterThe@gmail.com>
2025-01-09 10:22:26 -0300

    feat: inserting created at time into the database, plus new checkages

--- src/Private/php/ServerSide/register-server.php
+++ src/Private/php/ServerSide/register-server.php
@@ -3,28 +3,34 @@
 session_start();
 
 require_once "../Database/Database.php";
-require_once "../Database/db-checkages.php";
+require_once "../Database/DatabaseCheckage.php";
+require_once "../Utils/dates.php";
 
 $name = trim($_POST["student-name"]);
 $ra = trim($_POST["student-ra"]);
 $course = trim($_POST["student-course"]);
+$reg_time = get_utc_time();
 
-if(!is_student_unregistered($ra)) {
+$dbc = new DatabaseCheckage();
+
+if($dbc->is_unregistered("student_ra", "student_tbl", "student_ra =", array($ra))) {
 	echo "Estudante já possui conta.";
 	exit(1);
 }
 
+unset($dbc);
+
 $db = new Database();
-$db->insert("student_tbl(student_name, student_ra, student_course)", array($name, $ra, $course));
+$db->insert("student_tbl(student_name, student_ra, student_course, created_at)", array($name, $ra, $course, $reg_time));
 
 $suid = $db->select("student_id", "student_tbl", "student_ra = ", array($ra));
 
-$_SESSION["uid"] = $suid[0];
+unset($db);
+
+$_SESSION["uid"] = ($suid->fetch_array(MYSQLI_NUM))[0];
 $_SESSION["sname"] = $name;
 $_SESSION["sra"] = $ra;
 $_SESSION["scourse"] = $course;
-
-unset($db);
 
 header("Location: ../Student/profile.php");
 
