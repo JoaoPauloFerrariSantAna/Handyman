51a42546d6196db5d54a69626f9e0bc684e357b4
Dante <truedevilhunterThe@gmail.com>
2025-01-09 10:19:35 -0300

    feat: now updating a new field in the database

--- src/Private/php/ServerSide/del-acc-server.php
+++ src/Private/php/ServerSide/del-acc-server.php
@@ -1,10 +1,14 @@
 <?php
 
+/* Could i get, somehow, access user id without using session_start? */
+
 require_once __DIR__ . "/../Database/Database.php";
+require_once __DIR__ . "/../Utils/dates.php";
 
 $db = new Database();
 
 $db->update("student_tbl", "student_active", '0', "student_ra =", array($_POST["sra"]));
+$db->update("student_tbl", "deleted_at", get_utc_time(), "student_ra =", array($_POST["sra"]));
 
 unset($db);
 
